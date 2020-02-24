48a94caaec3e55ea28df3b0622bb1e6aef4705aa
Adam Willett <adam.willett@gmail.com>
2020-02-23 22:32:55 -0800

    Adding initial state and list items. Adding styles and fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "normalize.css";
+import "./fonts.css";
 import "./styles.css";
 
 export default function App() {
+  const [data, setData] = useState({ hits: [] });
+
   return (
     <div className="App">
       <h1>React Hooks: Fetching Data</h1>
       <h2>Using useState and useEffect asynchronously</h2>
       <p>Main body text.</p>
+
+      {/* data list from Hacker news */}
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
